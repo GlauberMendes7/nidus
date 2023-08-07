9beb74f0a64f248c134f1a73577f1e89a069618a
Felipe Toledo G Oliveira <felipetgoliveira@gmail.com>
2023-08-07 10:48:29 -0400

    Corrigido measurer

--- run.sh
+++ run.sh
@@ -5,4 +5,9 @@ python -m nidus --config=config.json node-1 &
 python -m nidus --config=config.json node-2 &
 python -m nidus --config=config.json node-3 &
 python -m nidus --config=config.json node-4 &
+
 python -m nidus --leader=localhost:12000 SET fruits apples 3
+python -m nidus --leader=localhost:12000 SET fruits oranges 5
+
+sleep 10
+pkill -9 python
